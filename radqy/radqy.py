f56961426cf130059911464fc4c9611d6664be07
Mohsen Hariri <mhariri68@gmail.com>
2024-07-18 22:44:12 -0400

    path resolver

--- radqy/radqy.py
+++ radqy/radqy.py
@@ -27,6 +27,38 @@ from scipy.io import loadmat
 import warnings
 warnings.filterwarnings("ignore")
 
+import importlib.resources
+from pathlib import Path
+
+
+def path_resolver(pkg, path):
+    """
+    Resolve the path to a resource in a package or a local script.
+    
+    Parameters
+    ----------
+    pkg : str
+        Name of the package.
+    path : str
+        Path to the resource.
+
+    Returns
+    -------
+    Path 
+        Path to the resource.
+    """
+    try:
+        return Path(importlib.resources.files(pkg)) / path
+    except TypeError:
+        return Path(path)
+    except FileNotFoundError:
+        raise FileNotFoundError(
+            f"Resource {path} not found in package {pkg} or locally."
+        )
+    except Exception as e:
+        raise e
+    
+
 def func1(F, B, c, f, b):
     name = 'MEAN'
     measure = np.nanmean(f)
@@ -555,17 +587,15 @@ if __name__ == '__main__':
     functions = [func for name, func in inspect.getmembers(sys.modules[__name__]) if name.startswith('func')]
     functions = sorted(functions, key=lambda f: int(re.search(r'\d+', f.__name__).group()))
 
+    tag_filename = path_resolver("radqy" , "MRI_TAGS.yaml") if scan_type == "MRI" else path_resolver("radqy" , "CT_TAGS.yaml")
+
+    with open(tag_filename, 'rb') as file:
+        tag_data = yaml.safe_load(file)
     
     if 'dicom' in df['subject_type'].values:
-        tag_filename = "MRI_TAGS.yaml" if scan_type == "MRI" else "CT_TAGS.yaml"
-        with open(tag_filename, 'rb') as file:
-            tag_data = yaml.safe_load(file)
         total_tags = sum(len(value) if isinstance(value, list) else 1 for value in tag_data.values())
         print(f'For each participant with dicom files, {total_tags} tags will be extracted and {len(functions) + 2} metrics will be computed.')
     else:
-        tag_filename = "MRI_TAGS.yaml" if scan_type == "MRI" else "CT_TAGS.yaml"
-        with open(tag_filename, 'rb') as file:
-            tag_data = yaml.safe_load(file)
         sample_image = sitk.ReadImage(df['path'][0]) if df['subject_type'][0] != 'dicom' else None
         sample_tags = extract_tags(sample_image, tag_data, file_type=df['subject_type'][0], image_shape=sitk.GetArrayFromImage(sample_image).shape)
         total_tags = len(sample_tags)
