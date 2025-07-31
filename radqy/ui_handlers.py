37f31006f4a5928c496ad9aadc4ba7a3e8cb8374
Amir R. Sadri <50635618+Amir-Reza-Sadri@users.noreply.github.com>
2025-07-31 10:59:58 -0400

    Add files via upload

---
+++ radqy/ui_handlers.py
@@ -0,0 +1,8 @@
+def ui_download():
+    print("UI download stub: implement if needed.")
+
+def ui_unzip():
+    print("UI unzip stub: implement if needed.")
+
+def ui_run():
+    print("UI run stub: implement if needed.")
